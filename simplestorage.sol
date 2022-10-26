ab6ba3aa2ac90d53b0001c12f45bde0e9f312631
delashoo <whoislilson@gmail.com>
2022-10-26 07:34:52 -0400

    first contract

---
+++ simplestorage.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17; // Instructing the compiler which solidity version to use
+
+contract SimpleStorage //every single smart contract has an address
+ {
+   uint256 favoritenumber;//initializes the uint256 to solidity's default, zero
+   People public person = People({favoritenumber: 24, name: "Delashoo"});// creating a new Person in the struct 
+
+   mapping (string => uint256) public nameToFavoriteNumber;
+
+   struct People {
+        uint256 favoritenumber;
+        string name;
+   }
+
+   People[] public people;//dynamic array bc the size is not given at initialization
+
+    function Store(uint256 _favNumber) public returns (uint256) {
+        favoritenumber = _favNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favoritenumber; //this is a view function, it only reads from the blockchain thus costs no gas fee
+    }
+
+    function addPerson(string memory _name, uint256 _favNumber) public returns () {
+        people.push(People(_favNumber, _name)); // Adding to the people array 
+        nameToFavoriteNumber[_name] = _favNumber;// adds to the mapping   
+    }
+}
